715ce51fdffc3f5db93dadc8b04d7662cd5c78c4
Maximus <maximuskatsoulis985@gmail.com>
2025-10-02 18:29:50 -0500

    Revealing and disabling features added

--- interactables/traps/spike_trap.gd
+++ interactables/traps/spike_trap.gd
@@ -1,20 +1,24 @@
 extends Node2D
-var mana_cost: int = 20
+var reveal_mana_cost: int = 20
+var disable_mana_cost: int = 40
 var disabled: bool = false
 var revealed: bool = false
 func _ready() -> void:
 	SignalManager.swapped_live_mode.connect(func(alive):
-		print("Swtitched")
 		if alive and not revealed:
 			visible = false
 		else:
 			visible = true
-			print("Showing")
 		)
+
+
 func handle_interact(player: Player) -> void:
-	print("Handling interact")
-	$Hitbox/CollisionShape2D.disabled = true
-	$Interactable/Area2D/CollisionShape2D.disabled = true
+	if not revealed:
+		player.consume_mana(reveal_mana_cost)
+		revealed = true
+		return
 	if not disabled:
-		player.consume_mana(mana_cost)
-	disabled = true
+		$Hitbox/CollisionShape2D.disabled = true
+		$Interactable/Area2D/CollisionShape2D.disabled = true
+		player.consume_mana(disable_mana_cost)
+		disabled = true
